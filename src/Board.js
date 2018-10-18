a168a708e219b985d24a4b099e44d838d0006e2a
AJ Magracia <ajmagracia@gmail.com>
2018-10-18 10:55:37 -0700

    more refactoring

--- src/Board.js
+++ src/Board.js
@@ -7,19 +7,18 @@ import Header from './Header'
 // A "Header", which is really just a message and name of the game,
 // Squares, which are just representations of the Board's state,
 // and the New Game button, which resets the game
-export default class Board extends React.PureComponent {
+export default class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      counter: 0,
-      message: "Click a square to get started",
       win: false,
       xo: [ "", "", "", "", "", "", "", "", "" ],
     }
   }
 
   render() {
-    const { xo, message } = this.state,
+    const { xo, win } = this.state,
+          counter = xo.join("").length,
           squares = xo.map(( square, index ) => {
       return (
         <Square
@@ -33,7 +32,7 @@ export default class Board extends React.PureComponent {
 
     return (
       <React.Fragment>
-        <Header message = { message }/>
+        <Header children = { this.announce( win, counter ) }/>
         <div className = "ttt">
           { squares }
         </div>
@@ -43,14 +42,14 @@ export default class Board extends React.PureComponent {
   }
 
   handleClick = ( index ) => {
-    const { counter, xo, win } = this.state,
-          { _update, _assignMarkers } = this
-
+    const { xo, win } = this.state
     // Only do something if the game is not yet won and space is not filled
-    if ( win || xo[ index ] !== "" ) return
+    if ( win !== false || xo[ index ] !== "" ) return
+    const { _update, _assignMarkers } = this,
+          counter = xo.join("").length,
+          current = _assignMarkers( counter )[0]
 
-    const [ current, next ] = _assignMarkers(counter)
-    _update( index, current, next, xo, counter + 1 )
+    _update( index, current, xo, counter + 1 )
   }
 
   // Assign values to current and next markers based on counter
@@ -59,28 +58,19 @@ export default class Board extends React.PureComponent {
   }
 
   // This function updates the xo array (used for checking win and displaying marker)
-  // It also updates counter (used for checking turn)
-  // It finally updates message (used for displaying turn)
-  _update = ( index, currentMarker, nextMarker, xo, counter ) => {
-    const { _checkWin } = this
+  // It then calls the checkWin function
+  _update = ( index, currentMarker, xo, counter ) => {
     xo[ index ] = currentMarker // This gets displayed on the Square
+    const { _checkWin } = this,
+          win = _checkWin( currentMarker, xo, counter )
 
-    this.setState({
-      xo,
-      counter,
-      message: `${nextMarker}'s turn`
-    }, () => {
-      _checkWin( currentMarker, xo, counter )
-    })
+    this.setState({ xo, win })
   }
 
-  // This function checks if a winning combination exists on the board
-  // If so, or a tie, announces appropriately
+  // This function checks/updates that a winning combination exists on the board
   _checkWin = ( marker, xo, counter ) => {
-    // Only win-check if winning is possible
-    if ( counter < 4 ) return
-    const { _announce } = this,
-          combos = [ // These arrays represent xo indexes of winning combinations
+    if ( counter < 5 ) return false // Only win-check if winning is possible
+    const combos = [ // These arrays represent xo indexes of winning combinations
       [ 0, 1, 2 ],
       [ 3, 4, 5 ],
       [ 6, 7, 8 ],
@@ -100,29 +90,26 @@ export default class Board extends React.PureComponent {
       })
     })
 
-    // If someone wins, alert such
-    if ( winnerExists ) return _announce( `${marker} Wins!!!` )
-    // Otherwise if there are no more moves and no winner, alert such
-    if ( counter === 9 ) _announce( "This is usually a tie, but it's 2018 so everyone's a winner" )
+    // If game ends, set state accordingly
+    if ( counter === 9 && !winnerExists ) return undefined
+    return winnerExists
   }
 
-  // This function updates win status (to prevent further moves)
-  // It also updates the message to announce the message (passed as argument)
-  _announce = ( what ) => {
-    this.setState({
-      win: true,
-      message: what,
-    })
+  // This method sets the message for Header to display
+  announce = (win, counter) => {
+    const [ next, current ] = this._assignMarkers(counter)
+    if ( counter === 0 ) return "Click a square to get started"
+    if ( win === undefined ) return "This is usually a tie, but it's 2018 so everyone's a winner"
+    if ( win ) return `${current} wins!`
+    return `${next}'s turn`
   }
 
-  // This function "resets" all state variables
+  // This function "resets" xo and win status
   // Because Squares are just representations of xo, this clears the board
   reset = () => {
     this.setState({
       xo: [ "", "", "", "", "", "", "", "", "" ],
-      win: false,
-      counter: 0,
-      message: "Click a square to get started",
+      win: false
     })
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header(props) {
     return(
       <React.Fragment>
         <h1 className="header">Tic Tac Toe</h1>
-        <h2 className="header">{ props.message }</h2>
+        <h2 className="header">{ props.children }</h2>
       </React.Fragment>
     )
 }
