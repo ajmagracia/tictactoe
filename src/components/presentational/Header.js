4e6f5e934bc533f8ccc485560b496591dcae1820
AJ Magracia <ajmagracia@gmail.com>
2018-10-18 14:37:29 -0700

    reformat

--- src/components/presentational/Header.js
+++ src/components/presentational/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function Header(props) {
+export default function Header({children}) {
   return(
     <React.Fragment>
       <h1 className="header">Tic Tac Toe</h1>
-      <h2 className="header">{ props.children }</h2>
+      <h2 className="header">{ children }</h2>
     </React.Fragment>
   )
 }
--- src/components/presentational/NewGame.js
+++ src/components/presentational/NewGame.js
@@ -2,14 +2,14 @@ import React from 'react'
 import '../../css/button.css'
 import PropTypes from 'prop-types'
 
-export default function NewGame(props) {
+export default function NewGame({reset}) {
   return(
     <div className="button">
-      <button onClick={props.reset} children="Reset Game" />
+      <button onClick={reset} children="Reset Game" />
     </div>
   )
 }
 
 NewGame.propTypes = {
-  onClick: PropTypes.func.isRequired
+  reset: PropTypes.func.isRequired
 }
